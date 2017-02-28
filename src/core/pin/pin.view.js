1226dfdc9dd697147e01c4830c9e161fd9179cb7
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-02-28 19:48:34 +0300

    init pin view

--- src/core/pin/pin.view.js
+++ src/core/pin/pin.view.js
@@ -1,7 +1,33 @@
 import View from 'core/view/view';
+import * as pinService from 'core/pin/pin.service';
+import Command from 'core/infrastructure/command';
+import * as columnService from 'core/column/column.service';
 
 export default class PinView extends View {
 	constructor(model) {
 		super(model);
+
+		this.toggle = new Command({
+			canExecute: key => {
+				const map = columnService.map(model.data().columns);
+				return map.hasOwnProperty(key);
+			},
+			execute: key => {
+				console.log(columnService.map(model.data().columns));
+				console.log('here should be pin for column with key', key);
+			}
+		});
+	}
+
+	state(key) {
+		const state = this.model.pin();
+		const by = state.by;
+		return pinService.index(by, key);
+	}
+
+	direction(key) {
+		const state = this.model.pin();
+		const by = state.by;
+		return pinService.map(by)[key];
 	}
 }
