0a56cd6304c81050d72fb52c959c019feffeb5cf
Andrey <munkk@yandex.ru>
2017-06-29 18:16:00 +0300

    Style-template-toolbar d.ts

---
+++ src/core/style/style.monitor.d.ts
@@ -0,0 +1,26 @@
+import * as css from '../services/css';
+import {Model} from "../infrastructure/model";
+
+export declare interface IClassResult{
+	(key: string, style: string): void;
+}
+
+export declare class Entry {
+	constructor(public element: HTMLElement, public sheets: Map);
+
+	list: Set;
+
+	class(key: string, style: string): void;
+}
+
+export declare class Monitor {
+	constructor(model: Model);
+
+	entries: Entry[];
+	newSheets: Map;
+	oldSheets: Map;
+
+	enter(): IClassResult;
+
+	exit(): void;
+}
