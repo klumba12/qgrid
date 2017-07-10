f795e168c99997e78fe31e4c9e61e092f422a077
Andrey <munkk@yandex.ru>
2017-07-10 20:17:22 +0300

    Fixed Action/Body/Cell/Column/Column-List d.ts

--- src/core/action/action.model.d.ts
+++ src/core/action/action.model.d.ts
@@ -1,9 +1,6 @@
-import {Resource} from '../resource/resource';
 import {Action} from '../infrastructure/action';
 
 export declare class ActionModel{
 	constructor();
-
-    resource: Resource;
 	items: Action[];
 }
