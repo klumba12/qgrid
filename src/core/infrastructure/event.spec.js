5830feb3354ed345fb84276c080e240166bf06a7
Andrey <munkk@yandex.ru>
2017-08-11 14:08:18 +0300

    Event test

--- src/core/infrastructure/event.spec.js
+++ src/core/infrastructure/event.spec.js
@@ -1,4 +1,39 @@
-describe('event', () => {
+import { Event } from './event';
 
+describe('Event', () => {
+	let event;
+
+	beforeEach(() => {
+		event = new Event();
+		window.testValue = 0;
+	});
+
+	let foo = function (value, func) {
+		func();
+		++window.testValue;
+	};
+
+	describe('on/emit', () => {
+
+		it('should return 1 in both cases', () => {
+			event.on(foo);
+			event.emit();
+			expect(window.testValue).to.equal(1);
+			event.emit();
+			expect(window.testValue).to.equal(1);
+		});
+
+	});
+
+	describe('watch', () => {
+
+		it('should return 0 if handler was removed', () => {
+			let result = event.watch(foo);
+			result();
+			event.emit();
+			expect(window.testValue).to.equal(0);
+		});
+
+	});
 
 });
