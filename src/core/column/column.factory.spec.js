c9182e86b80fc97e7b06b47240c07349e2f37c31
munkk <munkk@yandex.ru>
2017-08-30 15:35:47 +0300

    Update column.factory.spec.js

--- src/core/column/column.factory.spec.js
+++ src/core/column/column.factory.spec.js
@@ -5,16 +5,25 @@ import {ColumnView as CustomColumn} from '../column-type/column.model.view';
 
 describe('columnFactory', () => {
 	let model = modelFactory();
+	let body = {title: 'pass', pin: '1234'};
 
 	it('should create a new instance of PasswordColumn', () => {
-		let foo = columnFactory(model);
-		let result = foo('password');
+		let createColumn = columnFactory(model);
+
+		let result = createColumn('password');
 		expect(result).to.be.an.instanceOf(PasswordColumn);
 	});
 
 	it('should create a new instance of CustomColumn if there is no such type in the ColumnType list', () => {
-		let foo = columnFactory(model);
-		let result = foo('someColumn');
+		let createColumn = columnFactory(model);
+		let result = createColumn('someColumn');
 		expect(result).to.be.an.instanceOf(CustomColumn);
+	});
+
+	it('should set values to column`s model if body is specified', () => {
+		let createColumn = columnFactory(model);
+		let result = createColumn('password', body);
+		expect(result.model.title).to.equal('pass');
+		expect(result.model.pin).to.equal('1234');
 	});
 });
